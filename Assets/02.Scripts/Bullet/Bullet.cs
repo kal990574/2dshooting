e6f652a562bd399aa0b7912c70427c7ddca51e6c
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-04 11:04:27 +0900

    feat: Bullet 가속도 설정

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float _speed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Speed Settings")]
+    public float StartSpeed = 1f;        
+    public float EndSpeed = 7f;         
+    public float AccelerationTime = 1.2f;
+    public float _currentSpeed;
+    private float _elapsedTime = 0f;
+
     void Start()
     {
-        
+        _currentSpeed = StartSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.up * Time.deltaTime * _speed);
+        // 가속
+        if (_elapsedTime < AccelerationTime)
+        {
+            _elapsedTime += Time.deltaTime;
+            float t = _elapsedTime / AccelerationTime;
+            _currentSpeed = Mathf.Lerp(StartSpeed, EndSpeed, t);
+        }
+        else
+        {
+            _currentSpeed = EndSpeed;
+        }
+
+        // 총알 이동
+        transform.Translate(Vector2.up * (Time.deltaTime * _currentSpeed));
     }
 }
