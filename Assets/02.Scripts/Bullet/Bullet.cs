4060498b559b673d966cd917aad7d386b7ae56ef
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-05 15:38:42 +0900

    feat: 베지어 곡선 기능 적용

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -6,7 +6,8 @@ public class Bullet : MonoBehaviour
     {
         Straight = 0,      // 직선
         SideToSide = 1,    // 좌우 이동
-        Rotating = 2       // 나선형
+        Rotating = 2,      // 나선형
+        Bezier = 3         // 베지어 곡선
     }
 
     [Header("속도 설정")]
@@ -30,11 +31,24 @@ public class Bullet : MonoBehaviour
 
     private float _startX;
     private float _horizontalDirection = 1f;
+    //bz
+    private Vector3 _p1, _p2, _p3, _p4;
+    private float _t;
+    private float _duration = 1.0f;
 
     void Start()
     {
         CurrentSpeed = StartSpeed;
         _startX = transform.position.x;
+
+        // 랜덤 
+        float randomSign = Random.value > 0.5f ? 1f : -1f;
+
+        _p1 = transform.position;
+        _p2 = new Vector3(8f * randomSign, -13f, 0f);
+        _p3 = new Vector3(2f * randomSign, 8f, 0f);
+        _p4 = new Vector3(0f, 30f, 0f);
+        _t  = 0f;
     }
 
     void Update()
@@ -65,6 +79,9 @@ public class Bullet : MonoBehaviour
                 break;
             case MovementMode.Rotating:
                 MoveRotating();
+                break;
+            case MovementMode.Bezier:
+                MoveBezier();
                 break;
         }
     }
@@ -107,6 +124,23 @@ public class Bullet : MonoBehaviour
         transform.position += Vector3.up * (CurrentSpeed * Time.deltaTime);
     }
     
+    private void MoveBezier()
+    {
+        if (_t <= 1f)
+        {
+            _t += Time.deltaTime / _duration;
+            transform.position = EvaluateBezier(_p1, _p2, _p3, _p4, _t);
+        }
+    }
+    private Vector3 EvaluateBezier(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, float t)
+    {
+        Vector3 a = Vector3.Lerp(p1, p2, t);
+        Vector3 b = Vector3.Lerp(p2, p3, t);
+        Vector3 c = Vector3.Lerp(p3, p4, t);
+        Vector3 d = Vector3.Lerp(a, b, t);
+        Vector3 e = Vector3.Lerp(b, c, t);
+        return Vector3.Lerp(d, e, t);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Hitbox"))
