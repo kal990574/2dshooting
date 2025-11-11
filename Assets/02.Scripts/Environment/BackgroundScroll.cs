166b698538fd4a3eac34dc87bebb5415de6fbc3e
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-11 13:45:53 +0900

    feat: backgroundscroll 적용

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BackgroundScroll : MonoBehaviour
 {
-    
+    public Material BackgroundMaterial;
+    public float ScrollSpeed = 0.1f;
+
+    private void Update()
+    {
+        Vector2 direction = Vector2.up;
+        BackgroundMaterial.mainTextureOffset += direction * ScrollSpeed * Time.deltaTime;
+    }
 }
