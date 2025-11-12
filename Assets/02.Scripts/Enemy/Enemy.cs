547c0a0dd7a55065366b181296e1e830f40b1291
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-12 16:07:36 +0900

    feat: bullet sound 적용

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -12,7 +12,7 @@ public class Enemy : MonoBehaviour
     [Header("폭발 프리팹")]
     public ParticleSystem ExplosionPrefab;
 
-    private int _score = 100;
+    private int _score = 3000;
 
     void Awake()
     {
